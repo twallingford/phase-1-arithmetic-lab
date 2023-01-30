e0df55f05845fbf21deb8104cfcd62adf7e7fcb8
Thomas Wallingford <tbwallingford@gmail.com>
2023-01-30 15:42:27 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 16;
+const num4 = 6;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()) + 1;
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 5, 20);
